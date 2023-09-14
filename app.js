73f71ba68d73c421ca4e907df01ce032241e566e
hashimkhan17 <hashimkhan1852@gmail.com>
2023-09-14 18:01:09 +0500

    Update

--- app.js
+++ app.js
@@ -4,19 +4,24 @@ const cors = require("cors");
 const axios = require("axios");
 const app = express();
 const path = require("path");
+const FormData = require("form-data");
 app.use(cors()); // Enable CORS for all routes
+
+//this is used for displaying static folder
 app.use("/public", express.static("public"));
+
+//this is use for displaying views
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "view"));
 
-app.use(express.json());
-
+//this is used defining multer
 const upload = multer({
   limits: {
     fileSize: 1000000,
   },
 });
 
+// here we are creating a starton api link
 const starton = axios.create({
   baseURL: "https://api.starton.io/v3",
   headers: {
@@ -26,18 +31,23 @@ const starton = axios.create({
 
 app.post("/upload", cors(), upload.single("file"), async (req, res) => {
   let data = new FormData();
-  const blob = new Blob([req.file.buffer], { type: req.file.mimetype });
-  data.append("file", blob, { filename: req.file.originalname });
+  console.log(req.file);
+  const buffer = Buffer.from(req.file.buffer); // Convert to Buffer
+
+  data.append("file", buffer, { filename: req.file.originalname });
   data.append("isSync", "true");
 
+  // this is function used for uploading image on IPFS
   async function uploadImageOnIpfs() {
     const ipfsImg = await starton.post("/ipfs/file", data, {
       headers: {
-        "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+        ...data.getHeaders(), // Set appropriate headers for form data
       },
     });
     return ipfsImg.data;
   }
+
+  // this function is used for uplaoding meta data
   async function uploadMetadataOnIpfs(imgCid) {
     const metadataJson = {
       name: `A Wonderful NFT`,
@@ -51,12 +61,13 @@ app.post("/upload", cors(), upload.single("file"), async (req, res) => {
     });
     return ipfsMetadata.data;
   }
-
+  // here we are defiing the network
   const SMART_CONTRACT_NETWORK = "polygon-mumbai";
   const SMART_CONTRACT_ADDRESS = "0x09f22b18224Bf623c7a753748c3Ec74357AED923";
   const WALLET_IMPORTED_ON_STARTON =
     "0x83b567D5937D72A4A2dC2AC48059604C1DA03Ef5";
 
+  // this is minting function
   async function mintNFT(receiverAddress, metadataCid) {
     const nft = await starton.post(
       `/smart-contract/${SMART_CONTRACT_NETWORK}/${SMART_CONTRACT_ADDRESS}/call`,
@@ -69,7 +80,9 @@ app.post("/upload", cors(), upload.single("file"), async (req, res) => {
     );
     return nft.data;
   }
+  // this is receiver address
   const RECEIVER_ADDRESS = "0xbfc4a4b5F11B4e520eD888972e8C8dDaed46220D";
+
   const ipfsImgData = await uploadImageOnIpfs();
   const ipfsMetadata = await uploadMetadataOnIpfs(ipfsImgData.cid);
   const nft = await mintNFT(RECEIVER_ADDRESS, ipfsMetadata.cid);
@@ -85,6 +98,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(5005, () => {
-  console.log("Server is running on port 5004");
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port  ${port}`);
 });
--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
 let file = null;
-
 const handleSubmit = async (event) => {
   event.preventDefault();
   console.log(file);
@@ -12,24 +11,42 @@ const handleSubmit = async (event) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await fetch("http://localhost:5005/upload", {
+    const response = await fetch("http://localhost:5000/upload", {
       method: "POST",
       body: formData,
     });
 
     if (response.ok) {
       const data = await response.json();
-      
-      console.log(data.cid);
-      console.log(data.transactionHash);
+
+      console.log("data.cid:", data.cid);
+      console.log("data.transactionHash:", data.transactionHash);
+
+      const transactionDetails = document.getElementById("transaction");
+      const imageContainer = document.getElementById("image-container");
+
+      // Display the image in the "image-container" div
+      if (data.cid) {
+        let img = document.createElement("img");
+        img.src = `https://${data.cid}.ipfs.dweb.link`;
+        img.innerHTML = "";
+        imageContainer.appendChild(img);
+      }
+
+      // Display the transaction details as a link in the "transaction-details" div
+      if (data.transactionHash) {
+        const transactionLink = document.createElement("a");
+        transactionLink.href = `https://mumbai.polygonscan.com/tx/${data.transactionHash}`;
+        transactionLink.textContent = "Transaction Details";
+        transactionDetails.innerHTML = "";
+        transactionDetails.appendChild(transactionLink);
+      }
+
       alert("File uploaded successfully!");
     } else {
-  
-      console.error("File upload failed with status:", response.status);
       alert("File upload failed.");
     }
   } catch (error) {
-    console.error("An error occurred:", error);0
     alert("An error occurred while uploading the file.");
   }
 };
@@ -40,8 +57,8 @@ function retrieveFile(event) {
   // console.log("Selected File:", selectedFile);
 }
 
-const pick1 = document.querySelector(".input1"); 
+const pick1 = document.querySelector(".input1");
 pick1.addEventListener("change", retrieveFile);
 
-const btn1 = document.querySelector(".btn1"); 
+const btn1 = document.querySelector(".btn1");
 btn1.addEventListener("click", handleSubmit);
